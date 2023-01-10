929aa0c969d359784658817f650c19568356f11b
nehemias <20121348@itla.edu.do>
2023-01-10 19:26:08 -0400

    Agregar archivos de proyecto.

---
+++ Conexion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+namespace agenda12
+{
+    internal class Conexion
+    {
+        public static SqlConnection Connection()
+        {
+            SqlConnection cn = new SqlConnection("Data Source=LAPTOP-HQ961OQ0\\PAPUCHITO;Initial Catalog=REGISTRO;Integrated Security=True");
+            cn.Open();
+            return cn;
+        }
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace agenda12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.texCorreo = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.texDireccion = new System.Windows.Forms.TextBox();
+            this.texMovil = new System.Windows.Forms.TextBox();
+            this.comboBoxGenero = new System.Windows.Forms.TextBox();
+            this.texFecha = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.texTelefono = new System.Windows.Forms.TextBox();
+            this.comboBoxEstadoCivil = new System.Windows.Forms.TextBox();
+            this.texApellido = new System.Windows.Forms.TextBox();
+            this.texNombre = new System.Windows.Forms.TextBox();
+            this.texId = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 263);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(601, 124);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.texId);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.texCorreo);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.texDireccion);
+            this.groupBox1.Controls.Add(this.texMovil);
+            this.groupBox1.Controls.Add(this.comboBoxGenero);
+            this.groupBox1.Controls.Add(this.texFecha);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.texTelefono);
+            this.groupBox1.Controls.Add(this.comboBoxEstadoCivil);
+            this.groupBox1.Controls.Add(this.texApellido);
+            this.groupBox1.Controls.Add(this.texNombre);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(601, 233);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(507, 204);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "Eliminar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(360, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(421, 21);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(37, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "correo";
+            // 
+            // texCorreo
+            // 
+            this.texCorreo.Location = new System.Drawing.Point(415, 37);
+            this.texCorreo.Name = "texCorreo";
+            this.texCorreo.Size = new System.Drawing.Size(154, 20);
+            this.texCorreo.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(184, 136);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(31, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "movil";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(0, 191);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "genero";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(184, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "direccion";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(0, 136);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "fecha de nacimiento";
+            // 
+            // texDireccion
+            // 
+            this.texDireccion.Location = new System.Drawing.Point(178, 207);
+            this.texDireccion.Name = "texDireccion";
+            this.texDireccion.Size = new System.Drawing.Size(154, 20);
+            this.texDireccion.TabIndex = 11;
+            // 
+            // texMovil
+            // 
+            this.texMovil.Location = new System.Drawing.Point(178, 152);
+            this.texMovil.Name = "texMovil";
+            this.texMovil.Size = new System.Drawing.Size(154, 20);
+            this.texMovil.TabIndex = 10;
+            // 
+            // comboBoxGenero
+            // 
+            this.comboBoxGenero.Location = new System.Drawing.Point(0, 207);
+            this.comboBoxGenero.Name = "comboBoxGenero";
+            this.comboBoxGenero.Size = new System.Drawing.Size(154, 20);
+            this.comboBoxGenero.TabIndex = 9;
+            // 
+            // texFecha
+            // 
+            this.texFecha.Location = new System.Drawing.Point(0, 152);
+            this.texFecha.Name = "texFecha";
+            this.texFecha.Size = new System.Drawing.Size(154, 20);
+            this.texFecha.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(184, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "telefono";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(184, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "estado civil";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Apellido";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nombre";
+            // 
+            // texTelefono
+            // 
+            this.texTelefono.Location = new System.Drawing.Point(178, 100);
+            this.texTelefono.Name = "texTelefono";
+            this.texTelefono.Size = new System.Drawing.Size(154, 20);
+            this.texTelefono.TabIndex = 3;
+            // 
+            // comboBoxEstadoCivil
+            // 
+            this.comboBoxEstadoCivil.Location = new System.Drawing.Point(178, 37);
+            this.comboBoxEstadoCivil.Name = "comboBoxEstadoCivil";
+            this.comboBoxEstadoCivil.Size = new System.Drawing.Size(154, 20);
+            this.comboBoxEstadoCivil.TabIndex = 2;
+            // 
+            // texApellido
+            // 
+            this.texApellido.Location = new System.Drawing.Point(3, 100);
+            this.texApellido.Name = "texApellido";
+            this.texApellido.Size = new System.Drawing.Size(154, 20);
+            this.texApellido.TabIndex = 1;
+            // 
+            // texNombre
+            // 
+            this.texNombre.Location = new System.Drawing.Point(6, 37);
+            this.texNombre.Name = "texNombre";
+            this.texNombre.Size = new System.Drawing.Size(154, 20);
+            this.texNombre.TabIndex = 0;
+            this.texNombre.TextChanged += new System.EventHandler(this.texNombre_TextChanged);
+            // 
+            // texId
+            // 
+            this.texId.Location = new System.Drawing.Point(467, 75);
+            this.texId.Name = "texId";
+            this.texId.Size = new System.Drawing.Size(34, 20);
+            this.texId.TabIndex = 22;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(464, 60);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(44, 13);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "Agenda";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 399);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox texTelefono;
+        private System.Windows.Forms.TextBox comboBoxEstadoCivil;
+        private System.Windows.Forms.TextBox texApellido;
+        private System.Windows.Forms.TextBox texNombre;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox texCorreo;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox texDireccion;
+        private System.Windows.Forms.TextBox texMovil;
+        private System.Windows.Forms.TextBox comboBoxGenero;
+        private System.Windows.Forms.TextBox texFecha;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox texId;
+    }
+}
+
